d8d7f22fb58772d0ec407e5a24e552fadb8feae9
Prerana Pradhan <73207274+preranaapradhan@users.noreply.github.com>
2023-09-12 10:57:23 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,7 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last anme: ")
+university = input("Enter your university name: ")
+rollNo = int(input("Enter your rollNo.: "))
+semester = int(input("Enter the semester: "))
+
+print(firstName, lastName, university, rollNo, semester)
